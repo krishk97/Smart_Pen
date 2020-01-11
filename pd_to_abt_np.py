d84e34f459d41cbe2a9c7cc13af146aa28ebf13f
krish97 <krish@kabra.com>
2020-01-11 12:32:11 -0800

    moved pd_to_abt_np function to py file

---
+++ pd_to_abt_np.py
@@ -0,0 +1,36 @@
+# Input is a Pandas table of ax, ay, az, gx, gy, gz over time
+## Output is a np table of ax, ay, az, gx, gy, gz and selected features
+import numpy as np
+import pandas as pd
+from scipy import stats, fft, fftpack
+
+
+def pd_to_abt_np(dataframe):
+    '''
+    
+    '''
+    sensor_data = dataframe.to_numpy()
+    print(sensor_data.shape)
+    #want to see (# of samples,6)
+    
+    means = np.mean(sensor_data, axis=0)
+    ranges = np.ptp(sensor_data, axis = 0)
+    stds = np.std(sensor_data, axis=0)
+    moment_3 = stats.moment(sensor_data, moment=3, axis=0)
+    
+    dcts = fftpack.dct(sensor_data, axis=0)
+    sorted_dcts = np.sort(dcts, axis=0)
+    max_dcts = sorted_dcts[0]
+    second_max_dcts = sorted_dcts[1]
+    
+    ffts = fft(sensor_data, axis=0)
+    sorted_ffts = np.sort(ffts,axis=0)
+    max_ffts = sorted_ffts[0]
+    second_max_ffts = sorted_ffts[1]
+    
+    to_be_stacked = (means, ranges, stds, moment_3, max_dcts, second_max_dcts, 
+                    max_ffts, second_max_ffts)
+    
+    abt_table = np.vstack(to_be_stacked)
+    
+    return abt_table
