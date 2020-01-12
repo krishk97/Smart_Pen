bd0c3a48287c0ee323a60479477e7a386524cb3e
krish97 <krish@kabra.com>
2020-01-11 16:48:12 -0800

    updated main.py

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ import cv2
 import pickle
 import pandas as pd
 from scipy import stats, fft, fftpack
-from read_dataset import record_data               
+from read_serial import read_serial
+from record_dataset import record_data   
+from array_to_abt_np import array_to_abt_np            
 from matplotlib import pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.models import Sequential, load_model, model_from_json
+import keras
+from keras.models import Sequential, load_model, model_from_json
 
 # 0) Load network model
 classes = {
@@ -49,90 +52,84 @@ read = read_serial(PORT,BAUDRATE)
 read.init_comms()
 
 while(True): # need serial comm activity boolean
-    
-    ############################
-    #   2) Read serial data    # 
-    ############################
-    read.record_data()
 
+    try: 
+        ############################
+        #   2) Read serial data    # 
+        ############################
+        data = record_data(read)
 
-
-
-
-    ############################
-    #   3) Pre-process data    # 
-    ############################
-    input_array =   ########################################## ENTER YOUR INPUT HERE ##################################
-    
-    
-    
-    ########################################
-    ## Identify digit using trained model ##
-    ########################################
-    
-    prediction = model.predict(input_array)
-    predi = prediction[0].argmax()             # get index of greatest confidence
-    digit = classes[predi]                     # identify digit
-    
-    
-    
-    ###########################################
-    ## Display prediction confidence results ##
-    ###########################################
-    
-    disp_x = 800
-    disp_y = 800
-    data_display = np.zeros((disp_x, disp_y, 3), np.uint8)       
-    positions = {                                            
-        'digit': (15, 400), # hand pose text
-        'fps': (15, 20), # fps counter
-        'null_pos': (200, 200) # used as null point for mouse control
-    }
-    
-    for i, pred in enumerate(prediction[0]):
-        # Draw confidence bar for each gesture
-        barx = positions['hand_pose'][0]
-        bary = 60 + i*60
-        bar_height = 20
-        bar_length = int(400 * pred) + barx # calculate length of confidence bar
-
-        # Make the most confidence prediction green
-        if i == predi:
-            colour = (0, 255, 0)
-        else:
-            colour = (0, 0, 255)
-
-        cv2.putText(data_display, "{}: {}".format(classes[i], pred), (positions['digit'][0], 30 + i*60), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
-        cv2.rectangle(data_display, (barx, bary), (bar_length, bary - bar_height), colour, -1, 1)
-        cv2.putText(data_display, "digit: {}".format(gesture), positions['digit'], cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
+        ############################
+        #   3) Pre-process data    # 
+        ############################
+        input_array =   array_to_abt_np(data)    
         
-        cv2.imshow('data', data_display)
-    
-    
-    
-    ####################
-    ## Display digits ##
-    ####################
-    
-    x = 600
-    y = 520
-    img = np.zeros((x, y, 3),np.uint8)+255
-
-    i = 0
-    j = 0
-
-    cv2.imshow('Notes',img)
-    k = cv2.waitKey(0)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img,chr(k),(80+i,100+j), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, cv2.LINE_AA) 
-    i += 20
-    if i >= 360:
-        j += 30
-        i = 0
+        
+        ############################################
+        # 4.1) Identify digit using trained model  #
+        ############################################
+        
+        prediction = model.predict(input_array)
+        predi = prediction[0].argmax()             # get index of greatest confidence
+        digit = classes[predi]                     # identify digit
             
-    if k == 27:        # press 'ESC' to quit
-        break
-
+        ###############################################
+        # 4.2) Display prediction confidence results  #
+        ###############################################
         
-cap.release()
-cv2.destroyAllWindows()
+        disp_x = 800
+        disp_y = 800
+        data_display = np.zeros((disp_x, disp_y, 3), np.uint8)       
+        positions = {                                            
+            'digit': (15, 400), # hand pose text
+            'fps': (15, 20), # fps counter
+            'null_pos': (200, 200) # used as null point for mouse control
+        }
+        
+        for i, pred in enumerate(prediction[0]):
+            # Draw confidence bar for each digit
+            barx = positions['hand_pose'][0]
+            bary = 60 + i*60
+            bar_height = 20
+            bar_length = int(400 * pred) + barx # calculate length of confidence bar
+
+            # Make the most confidence prediction green
+            if i == predi:
+                colour = (0, 255, 0)
+            else:
+                colour = (0, 0, 255)
+
+            cv2.putText(data_display, "{}: {}".format(classes[i], pred), (positions['digit'][0], 30 + i*60), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
+            cv2.rectangle(data_display, (barx, bary), (bar_length, bary - bar_height), colour, -1, 1)
+            cv2.putText(data_display, "digit: {}".format(digit), positions['digit'], cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
+            
+            cv2.imshow('data', data_display)
+        
+        
+        #######################
+        # 5) Display digits   #
+        #######################
+        
+        x = 600
+        y = 520
+        img = np.zeros((x, y, 3),np.uint8)+255
+
+        i = 0
+        j = 0
+
+        cv2.imshow('Notes',img)
+
+        # key = cv2.waitKey(0)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        # cv2.putText(img,chr(k),(80+i,100+j), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(img,chr(digit),(80+i,100+j), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, cv2.LINE_AA)  
+        i += 20
+        if i >= 360:
+            j += 30
+            i = 0
+            
+    except KeyboardInterrupt: 
+        print('Exiting!')
+        cv2.destroyAllWindows()
+        read.close_comms()
+        break
